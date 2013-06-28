52888c09a4e03ca1443f2392a3fbe86e39defe5b
Vitor Freitas <vitorfs@gmail.com>
2013-06-28 18:46:56 -0300

    Initial commit

---
+++ ExemploLocadora/src/br/edu/ufjf/model/Item.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ufjf.model;
+
+/**
+ *
+ * @author vitorfs
+ */
+public class Item {
+    
+}
