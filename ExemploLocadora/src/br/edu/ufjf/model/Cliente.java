49357d2a8037e3732532f1736cf75f1ba246401d
Vitor Freitas <vitorfs@gmail.com>
2013-07-15 14:52:55 -0300

    creating the program interface

--- ExemploLocadora/src/br/edu/ufjf/model/Cliente.java
+++ ExemploLocadora/src/br/edu/ufjf/model/Cliente.java
@@ -9,5 +9,15 @@ package br.edu.ufjf.model;
  * @author vitorfs
  */
 public class Cliente {
-    
+    private Integer id;
+    private String nome;
+    private String email;
+    private String telefoneResidencial;
+    private String localTrabalho;
+    private String telefoneComercial;
+    private String telefoneCelular;
+    private String sexo;
+    private String cpf;
+    private String dataNascimento;
+    private String endereco;
 }
